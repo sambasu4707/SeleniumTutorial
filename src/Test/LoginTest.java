14154552f7aac9b9fcda97e806373a5c6e537dd5
Samadrita Basu <samadritabasu@Samadritas-MacBook-Air.local>
2022-04-18 00:05:43 +0530

    third commit

--- src/Test/LoginTest.java
+++
@@ -1,8 +0,0 @@
-package Test;
-
-public class LoginTest {
-
-	public void test1() {
-		System.out.println("firstTest");
-	}
-}
---
+++ src/Test/OpportunityTest.java
@@ -0,0 +1,9 @@
+package Test;
+
+public class OpportunityTest {
+	
+	private void Syssout() {
+		// TODO Auto-generated method stub
+			System.out.println("String");	}
+
+}
